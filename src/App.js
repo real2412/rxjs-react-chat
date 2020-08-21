311d4ebef82e5212827c59d92fbb35841d92b427
Renan <ramsp2412@gmail.com>
2020-08-21 09:03:30 -0500

    feat: add code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { FirstPerson, SecondPerson, PersonSwitcher } from './components';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <>
+      <PersonSwitcher />
+      <Switch>
+        <Route path="/" component={FirstPerson} exact />
+        <Route path="/first-person" component={FirstPerson} exact />
+        <Route path="/second-person" component={SecondPerson} exact />>
+      </Switch>
+    </>
+  </ BrowserRouter>
+)
 
 export default App;
